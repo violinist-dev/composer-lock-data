95fe179fa6b6b3b27afe5d127e1f4f3e83aff733
Eirik S. Morland <eirik@morland.no>
2019-08-04 16:36:26 +0200

    More tests

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -14,4 +14,42 @@ class UnitTest extends TestCase
         $package_data = $data->getPackageData('phpunit/phpunit');
         $this->assertEquals('phpunit/phpunit', $package_data->name);
     }
+
+    public function testInvalidPackage()
+    {
+        $data = ComposerLockData::createFromFile(__DIR__ . '/../../composer.lock');
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Did not find the requested package (derp/error) in the lockfile. This is probably an error');
+        $data->getPackageData('derp/error');
+    }
+
+    public function testInvalidPath()
+    {
+        $path = __DIR__ . '/../../composer-non-existing.lock';
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('No composer.lock data at filepath ' . $path);
+        $data = ComposerLockData::createFromFile($path);
+    }
+
+    public function testEmptyJson()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The supposed composer.lock data was empty');
+        ComposerLockData::createFromString('');
+    }
+
+    public function testBadJson()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The data provided was not valid JSON');
+        ComposerLockData::createFromString('{bad_json: true');
+    }
+
+    public function testGetData()
+    {
+        $cdata = ComposerLockData::createFromFile(__DIR__ . '/../../composer.lock');
+        $data = $cdata->getData();
+        $this->assertEquals('stable', $data->{"minimum-stability"});
+    }
+
 }
